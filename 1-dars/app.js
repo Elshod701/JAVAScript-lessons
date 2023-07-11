745fa03d6655a5cb8fd160749195b9c13004d541
Elshod701 <jorayevelshod511@gmail.com>
2023-07-11 15:36:48 +0500

    first lesson

---
+++ 1-dars/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// O'zgaruvchilar
+
+// 1
+let myName = "Elshod";
+let age = 20;
+console.log(myName, age);
+age = 21;
+console.log(age);
+// qayta qiymat tayinlash mumkin emas, lekin qayta o'zgartirish mumkin
+
+// 2
+const capital = "Tashkent";
+console.log(capital);
+// o'zgarmas o'zgaruvchi
+
+// 3
+var oldVariable = 14;
+oldVariable = 12;
+console.log(oldVariable);
+// eski ! Ishlatish tavsiya qilinmaydi
